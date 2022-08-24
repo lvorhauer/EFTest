c0e362701dbf42b9495abdf9b30389edff5cfe71
lvorhauer <70147926+lvorhauer@users.noreply.github.com>
2022-08-24 12:20:14 +0200

    Add files via upload

---
+++ TestAppEF/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestAppEF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var context = new UserContext();
+
+            context.Database.Migrate();
+        }
+
+    }
+}
---
+++ TestAppEF/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppEF
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlite(@"Data Source=Users.db;");
+        }
+    }
+
+}
+
